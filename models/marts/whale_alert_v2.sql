663b53d10807bc190864ab9da36cc3e11cd04998
Daniel Weigel <d.weigel@reply.com>
2025-08-09 16:13:59 +0200

    latest

---
+++ models/marts/whale_alert_v2.sql
@@ -0,0 +1,26 @@
+
+
+WITH WHALES as (
+
+select
+output_address,
+sum(output_value) as total_sent,
+count(*) as tx_count
+
+from {{ ref('stg_btc_transactions')}}
+
+where output_value > 10
+
+group by output_address
+order by total_sent desc
+)
+
+select
+'{{ invocation_id }}' as invocation_id,
+w.output_address,
+w.total_sent,
+w.tx_count
+
+from WHALES w
+
+order by total_sent desc
